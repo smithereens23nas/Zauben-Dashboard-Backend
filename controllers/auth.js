a634bc953d14b9708137897a3765602772326a06
smiethereens23nas <nicholas23smith@gmail.com>
2022-03-25 14:17:33 -0400

    add salt and hashing to the post route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,25 @@
 const express = requrie("express");
 const router = express();
 const {User} = require("../models");
-const verifyAuth = require("../controllers/verifyAuth")
+// const verifyAuth = require("../controllers/verifyAuth")
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+require("dotenv").config()
+
+router.post("/register", async (req, res) => {
+    const user = req.body;
+    try {
+        const foundUser = await User.exists({email: req.body.email});
+        if (foundUser) {
+            return res.status(400).json(error)
+        }
+        const salt = await bcrypt.genSalt(12);
+        const hash = await bcrypt. hash(req.body.password, salt);
+        req.body.password = hash;
+        res.json(await User.create(user));
+
+    } catch (error){
+        res.status(400).json(error);
+    }
+});
+
